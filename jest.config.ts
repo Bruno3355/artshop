35382b7d03bb351d0551cddc2e613484839b569a
Bruno3355 <brunohdossantos3@hotmail.com>
2026-09-08 20:38:52 -0300

    Jest bugfix

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
+  coverageReporters: ["lcov", "text", "html"],
+  coverageDirectory: "coverage",
 };
 
 export default config;
